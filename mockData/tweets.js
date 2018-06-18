9ac9c3b2dcd6a3866a2f56e5d64c51babfa173af
Chris <fnatikk@gmail.com>
2018-06-19 00:18:43 +0200

    Add more real tweets

--- mockData/tweets.js
+++ mockData/tweets.js
@@ -1,30 +1,30 @@
 export default Tweets = [
   {
-    image: 'https://pbs.twimg.com/media/Bm54nBCCYAACwBi.jpg',
-    authorImage: 'https://pbs.twimg.com/profile_images/432081479/DOI_LOGO_bigger.jpg',
-    author: 'US Department of the Interior',
-    account: '@Interior',
-    message: 'Sunsets don\'t get much better than this one over @GrandTetonNPS. #nature #sunset',
-    date: '12:09 AM - May 6, 2014',
-    likes: 4891,
-    retweets: '4,179 people are talking about this'
+    image: 'https://pbs.twimg.com/tweet_video_thumb/DfLsWoFXUAE-sjd.jpg',
+    authorImage: 'https://pbs.twimg.com/profile_images/633655578108272641/GuajM-lf_bigger.jpg',
+    author: 'RuhrJS',
+    account: '@RuhrJS',
+    message: '🌟 The first early-bird badge is now online 🌟 We also opened our supporter Tickets for the regular price incl. the % amount you support us to create a more diverse audience',
+    date: '07:00 PM - June 8, 2018',
+    likes: 15,
+    retweets: '26 people are talking about this'
   }, {
-    image: 'https://pbs.twimg.com/media/Bm54nBCCYAACwBi.jpg',
-    authorImage: 'https://pbs.twimg.com/profile_images/432081479/DOI_LOGO_bigger.jpg',
-    author: 'US Department of the Interior',
-    account: '@Interior',
-    message: 'Sunsets don\'t get much better than this one over @GrandTetonNPS. #nature #sunset',
-    date: '12:09 AM - May 6, 2014',
-    likes: 4891,
-    retweets: '4,179 people are talking about this'
+    image: 'https://pbs.twimg.com/tweet_video_thumb/Df5cUg6XkAA4vqK.jpg',
+    authorImage: 'https://pbs.twimg.com/profile_images/633655578108272641/GuajM-lf_bigger.jpg',
+    author: 'RuhrJS',
+    account: '@RuhrJS',
+    message: 'Wow! The first round of early birds is sold-out! The next badge will go online on Monday, 25th June, 11am CEST!',
+    date: '03:46 PM - June 17, 2018',
+    likes: 2,
+    retweets: '1 people are talking about this'
   }, {
-    image: 'https://pbs.twimg.com/media/Bm54nBCCYAACwBi.jpg',
-    authorImage: 'https://pbs.twimg.com/profile_images/432081479/DOI_LOGO_bigger.jpg',
-    author: 'US Department of the Interior',
-    account: '@Interior',
-    message: 'Sunsets don\'t get much better than this one over @GrandTetonNPS. #nature #sunset',
-    date: '12:09 AM - May 6, 2014',
-    likes: 4891,
-    retweets: '4,179 people are talking about this'
+    image: 'https://pbs.twimg.com/tweet_video_thumb/DfvHmZnW0AAMGpP.jpg',
+    authorImage: 'https://pbs.twimg.com/profile_images/633655578108272641/GuajM-lf_bigger.jpg',
+    author: 'RuhrJS',
+    account: '@RuhrJS',
+    message: '🎉🎉🎉 Our Lineup is now fully announced and we\'re absolutely delighted about all the awesome people! Check it out! 🎉🎉🎉 https://www.ruhrjs.de/speakers',
+    date: '03:39 AM - June 15, 2018',
+    likes: 5,
+    retweets: '5 people are talking about this'
   }
 ]
