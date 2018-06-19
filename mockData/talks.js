88e667b3f18853e7b58a00a966fd6dbd70597628
Chris <fnatikk@gmail.com>
2018-06-19 15:24:36 +0200

    Add talks mock

---
+++ mockData/talks.js
@@ -0,0 +1,83 @@
+export default Talks = [
+  {
+    start_time: '09:30',
+    end_time: '10:30',
+    speaker: '',
+    talk: 'Breakfast'
+  }, {
+    start_time: '10:30',
+    end_time: '10:35',
+    speaker: '',
+    talk: 'Welcoming'
+  }, {
+    start_time: '11:05',
+    end_time: '11:35',
+    speaker: 'Sara Harkousse',
+    talk: 'Web Components: It\'s all rainbows and unicorns! Is it?'
+  }, {
+    start_time: '11:10',
+    end_time: '11:40',
+    speaker: 'Jessica Jordan',
+    talk: 'Animate the Web with Ember.js'
+  }, {
+    start_time: '11:40',
+    end_time: '12:00',
+    speaker: '',
+    talk: 'Coffee Break'
+  }, {
+    start_time: '12:00',
+    end_time: '12:30',
+    speaker: 'Szabolcs Szabolcsi-Toth',
+    talk: 'Redundant ops - about reinventing the wheel'
+  }, {
+    start_time: '12:35',
+    end_time: '13:05',
+    speaker: 'Stefan Judis',
+    talk: 'Watch your back, Browser! You\'re being observed'
+  }, {
+    start_time: '13:05',
+    end_time: '14:00',
+    speaker: '',
+    talk: 'Lunch'
+  }, {
+    start_time: '14:00',
+    end_time: '14:30',
+    speaker: 'Michaela Lehr',
+    talk: 'How to make your user sick in 60 seconds – About UX design challenges when...'
+  }, {
+    start_time: '14:35',
+    end_time: '15:05',
+    speaker: 'Anjana Vakil',
+    talk: 'Immutable data structures for functional JS'
+  }, {
+    start_time: '15:05',
+    end_time: '15:20',
+    speaker: '',
+    talk: 'Coffee Break'
+  }, {
+    start_time: '15:20',
+    end_time: '15:50',
+    speaker: 'Rachel Andrew',
+    talk: 'Start Using CSS Grid Layout Today'
+  }, {
+    start_time: '15:55',
+    end_time: '16:25',
+    speaker: 'Lisa Passing',
+    talk: 'An adequate intro to ClojureScript'
+  }, {
+    start_time: '16:25',
+    end_time: '16:40',
+    speaker: '',
+    talk: 'Coffee Break & Family-Photo'
+  }, {
+    start_time: '16:40',
+    end_time: '17:10',
+    speaker: 'Lian Li',
+    talk: 'Alpha 2048 - Reinforcement Learning in the Browser'
+  }, {
+    start_time: '17:10',
+    end_time: '',
+    speaker: '',
+    talk: 'Farewell'
+  }
+]
